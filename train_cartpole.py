a868caa1deb6c76f027fe56fecc34f5311a6f29b
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-09-03 18:13:30 -0700

    More cartpole cleanup

--- train_cartpole.py
+++ train_cartpole.py
@@ -18,45 +18,19 @@ EPSILON = 0.1
 GAMMA = 0.9
 TARGET_UPDATE_INTERVAL = 10
 REPORT_INTERVAL = 100
-RENDER_INTERVAL = 0
-
-class Episode:
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.memory = []
-
-    def push(self, *args):
-        self.memory.append(tuple(args))
-
-    def discount_rewards(self, gamma, normalize = False):
-        running_add = 0.
-        total_reward = 0.
-        for i, (_, _, reward, *_) in list(enumerate(self.memory))[::-1]:
-            running_add = running_add * gamma + reward
-            total_reward += running_add
-
-        running_add = 0.
-        avg_reward = total_reward / len(self.memory) if normalize else 0.
-        for i, (state, action, reward, *rest) in list(enumerate(self.memory))[::-1]:
-            running_add = running_add * gamma + reward
-            discounted_reward = torch.tensor([running_add - avg_reward])
-            self.memory[i] = (state, action, discounted_reward, *rest)
-
+RENDER_INTERVAL = 100
 
 class ReplayMemory:
     def __init__(self, capacity = None):
         self.capacity = capacity
         self.memory = []
-    
+
     def __len__(self):
         return len(self.memory)
 
     def push_episode(self, episode):
-        for step in episode.memory:
+        for step in episode:
             self.push_transition(step)
-        episode.reset()
 
     def push_transition(self, transition):
         if self.capacity is None or len(self.memory) < self.capacity:
@@ -81,20 +55,42 @@ class CartPoleAgent(nn.Module):
         x = F.relu(self.fc2(x))
         return F.softmax(self.output(x), dim=-1)
 
+screen = None
+def draw(img):
+    import pygame
+    global screen
+    if not screen:
+        pygame.init()
+        screen = pygame.display.set_mode((img.shape[1], img.shape[0]))
+    img = env.render().transpose(1, 0, 2)
+    screen.blit(pygame.surfarray.make_surface(img), (0, 0))
+    pygame.display.flip()
+
+def discount_rewards(episode, gamma, normalize=False):
+    discounted_returns = 0.
+    for i, (state, action, reward, *rest) in list(enumerate(episode))[::-1]:
+        discounted_returns = reward + gamma * discounted_returns
+        episode[i] = (state, action, torch.tensor([discounted_returns]), *rest)
+    if normalize:
+        rewards = torch.stack([r for _, _, r, *_ in episode], 0)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        for i, (state, action, _, *rest) in enumerate(episode):
+            episode[i] = (state, action, rewards[i].clone(), *rest)
+
 
 # Training
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v1')
+    env = gym.make('CartPole-v1', render_mode='rgb_array')
     agent = CartPoleAgent()
     old_agent = CartPoleAgent()
     optimizer = torch.optim.Adam(agent.parameters(), lr=LEARNING_RATE)
 
-    episode = Episode()
     memory = ReplayMemory(MEMORY_SIZE)
     episode_lengths, avg_episode_length = [], 0
 
     for episode_counter in range(1, NUM_EPISODES + 1):
+        episode = []
         state, _ = env.reset()
         state = torch.from_numpy(state)
 
@@ -104,16 +100,16 @@ if __name__ == '__main__':
                 policy = agent(state.view(1, -1))
             action = Categorical(policy).sample()
             next_state, reward, done, _, stats = env.step(action.item())
-            episode.push(state, action, reward)
+            episode.append((state, action, reward))
             state = torch.from_numpy(next_state)
 
             if RENDER_INTERVAL and episode_counter % RENDER_INTERVAL == 0:
-                env.render()
+                draw(env.render())
             if done: break
 
         # When the episode finishes, reset the environment and update the agent
         episode_lengths.append(step + 1.)
-        episode.discount_rewards(GAMMA)
+        discount_rewards(episode, GAMMA, normalize=False)
         memory.push_episode(episode)
 
         if len(memory) > BATCH_SIZE * 4:
@@ -122,7 +118,7 @@ if __name__ == '__main__':
 
             inputs, actions, rewards = memory.sample(BATCH_SIZE)
             rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
-            
+
             outputs = agent(inputs)
             with torch.no_grad():
               old_outputs = old_agent(inputs)

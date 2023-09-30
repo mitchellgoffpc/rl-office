f90c6c3f25d18c12bfa1a3554d9ad813e053d2b4
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-09-30 15:11:36 -0700

    actor critic works great!

--- train_torch.py
+++ train_torch.py
@@ -8,7 +8,7 @@ from collections import deque
 from environment import BodyEnvironment
 from wrappers import FrameStack
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0003
 BATCH_SIZE = 128
 REPLAY_SIZE = 128000
 NUM_EPISODES = 1000
@@ -18,18 +18,23 @@ REPORT_INTERVAL = 50
 TARGET_UPDATE_INTERVAL = 10
 GAMMA = 0.9
 
-class DQN(nn.Module):
+class ActorCritic(nn.Module):
     def __init__(self):
-        super(DQN, self).__init__()
+        super(ActorCritic, self).__init__()
         self.fc1 = nn.Linear(HISTORY_LEN, 64)
-        self.fc2 = nn.Linear(64, 4)
+        self.fc2 = nn.Linear(64, 64)
+        self.actor = nn.Linear(64, 4)
+        self.critic = nn.Linear(64, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        x = F.relu(self.fc1(x / 100))
+        x = F.relu(self.fc2(x))
+        return F.softmax(self.actor(x), dim=-1), self.critic(x)
 
 class ReplayBuffer:
     def __init__(self, capacity):
+        # self.capacity = capacity
+        # self.buffer = []
         self.buffer = deque(maxlen=capacity)
 
     def __len__(self):
@@ -37,6 +42,10 @@ class ReplayBuffer:
 
     def push(self, *args):
         self.buffer.append(args)
+        # if len(self.buffer) < self.capacity:
+        #     self.buffer.append(args)
+        # else:
+        #     self.buffer[max(np.random.randint(len(self.buffer)), np.random.randint(len(self.buffer)))] = args
 
     def sample(self, batch_size):
         state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
@@ -44,30 +53,28 @@ class ReplayBuffer:
 
 
 def main():
-    model = DQN()
-    target_model = DQN()
+    model = ActorCritic()
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
     env = BodyEnvironment()
     env = FrameStack(env, HISTORY_LEN)
     buffer = ReplayBuffer(REPLAY_SIZE)
+
     loss_deque = deque(maxlen=REPORT_INTERVAL)
     reward_deque = deque(maxlen=REPORT_INTERVAL)
     bumps_deque = deque(maxlen=REPORT_INTERVAL)
+    total_bumps = 0
 
     for episode in range(1, NUM_EPISODES):
-        epsilon = max(0.05, 1 - episode / 200)
         state = env.reset()
         ep_loss, ep_reward, ep_bumps = 0, 0, 0
 
         for step in range(MAX_EPISODE_LEN):
-            if random.random() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = model(torch.tensor(state)[None]).argmax().item()
+            with torch.no_grad():
+                probs, _ = model(torch.tensor(state)[None])
+            action = np.random.choice(4, p=probs.numpy()[0])
 
             next_state, reward, done, _ = env.step(action)
-            buffer.push(state, action, reward, next_state, done or step == MAX_EPISODE_LEN - 1)
+            buffer.push(state, action, reward, next_state, done)
             state = next_state
             ep_reward += reward
             ep_bumps += 1 if reward == -1 else 0
@@ -75,14 +82,16 @@ def main():
 
             if len(buffer) >= BATCH_SIZE:
                 states, actions, rewards, next_states, dones = (torch.tensor(x) for x in buffer.sample(BATCH_SIZE))
-                state_action_values = model(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)
-                with torch.no_grad():
-                    next_state_values = target_model(next_states).max(1)[0]
-                    # double dqn, doesn't work as well :(
-                    # next_state_actions = model(next_states).max(1)[1]
-                    # next_state_values = target_model(next_states)[torch.arange(len(next_state_actions)), next_state_actions]
-                expected_state_action_values = (~dones) * next_state_values * GAMMA + rewards
-                loss = F.mse_loss(state_action_values, expected_state_action_values)
+                probs, state_values = model(states)
+                _, next_state_values = model(next_states)
+                target_values = (rewards + (~dones) * next_state_values.squeeze(-1) * GAMMA)[:,None]
+                critic_loss = F.mse_loss(state_values, target_values)
+
+                action_probs = probs[range(BATCH_SIZE), actions][:,None]
+                action_probs = torch.clip(action_probs, 0.01, 1)  # probs nan out without this :(
+                advantages = (target_values - state_values).detach()
+                actor_loss = -torch.mean(torch.log(action_probs) * advantages)
+                loss = actor_loss + critic_loss
                 ep_loss += loss.item()
 
                 optimizer.zero_grad()
@@ -92,12 +101,14 @@ def main():
         loss_deque.append(ep_loss)
         reward_deque.append(ep_reward)
         bumps_deque.append(ep_bumps)
+        total_bumps += ep_bumps
 
         if episode % REPORT_INTERVAL == 0:
-            print(f"Episode: {episode} | Loss: {np.mean(loss_deque):.3f} | Reward: {np.mean(reward_deque):.3f} | Bumps: {np.mean(bumps_deque):.3f}")
-
-        if episode % TARGET_UPDATE_INTERVAL == 0:
-            target_model.load_state_dict(model.state_dict())
+            print(f"Episode: {episode} | "
+                  f"Loss: {np.mean(loss_deque):.3f} | "
+                  f"Avg Reward: {np.mean(reward_deque):.3f} | "
+                  f"Avg Bumps: {np.mean(bumps_deque):.3f} | "
+                  f"Total Bumps: {total_bumps}")
 
 
 if __name__ == "__main__":
